18f31c4be4c992a5fcdc2e16f3752453f5a23446
Lucas Carioca <lucascarioca97@gmail.com>
2022-04-14 23:24:33 -0300

    adjustements for typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,13 +3,19 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
-        fetch('https://api.github.com/orgs/rocketseat/repos')// https://api.github.com/users/lucasscarioca/repos
-        .then(response => response.json())
-        .then(data => setRepositories(data))
+        fetch('https://api.github.com/users/lucasscarioca/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
     }, []);
 
     return (
